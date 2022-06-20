45332edf0e9578d6ea5e719c6721c696b9be053a
MariaVilcapoma <i2011451@continental.edu.pe>
2022-06-20 07:36:54 -0500

    cambio

--- pages/empleado.tsx
+++ pages/empleado.tsx
@@ -24,57 +24,34 @@ export default function Example() {
     const [open, setOpen] = useState(false)
     const [activo, setActivo] = useState(true)
     const [idEmpleado, setIdEmpleado] = useState(-1)
+    const [numActivo, setnumActivo] = useState(0)
     const { data } = useSWRImmutable(['/api/empleado', {}], fetcherEmpleado)
+    const [filtrotodos, setFiltrotodos] = useState(false)
+
+    // function AxInput() {
+    //     return (
+    //         <>
+    //             {data && data.map((empleado) => (
+
+    //                 setnumActivo(empleado + empleado)
+
+    //             ))}
+    //         </>
+    //     )
+    // }
+
+    // data && data.forEach((empleado) => (
+
+    //     console.log( count(empleado))
+
+    // ))
+
+    // console.log(numActivo)
     // const [count, setCount] = useState(0);
     // const {numero}= React.data.count(data);
     //   const {numero}= count(data:any[]);
     // console.log(count(data && data):any[]);
     //   console.count(data);
-    function AxImput() {
-        return (
-            <>
-                <ul role="list" className="divide-y divide-gray-200">
-                    {data && data.map((empleado) => (
-                        <li key={empleado.id}>
-                            <a onClick={() => { setIdEmpleado(empleado.id) }}
-                                className={(empleado.id == idEmpleado ? "bg-indigo-100" : "") + " block hover:bg-indigo-200"}>
-                                <div className="flex px-4 py-4 sm:px-6">
-                                    <div className="min-w-0 flex-1 flex">
-                                        <div className="flex-shrink-0">
-                                            {
-                                                empleado.ImgURL
-                                                    ? <img className="h-12 w-12 rounded-full" src={empleado.ImgURL} alt="" />
-                                                    : <svg className="bg-indigo-300 text-white h-12 w-12 rounded-full" viewBox="0 0 20 20" fill="currentColor">
-                                                        <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
-                                                    </svg>
-                                            }
-
-                                        </div>
-                                        <div className="min-w-0 flex-1 px-4 md:grid md:grid-cols md:gap-4">
-                                            <div>
-                                                <p className="text-sm font-medium text-indigo-600 truncate">{empleado.Nombres + ' ' + empleado.Apellidos}</p>
-                                                <p className="mt-2 flex text-sm text-gray-500">
-                                                    <MailIcon className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
-                                                    <span className="truncate">{empleado.Email}</span>
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <ChevronRightIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                                    </div>
-                                </div>
-                            </a>
-                        </li>
-                    ))}
-                </ul>
-            </>
-        )
-    }
-    //     if (element.EsActivo == false) {
-    //         console.log(element.Nombres)
-    //     }
-    // });
     return (
         <>
             <div className="h-full flex flex-col">
@@ -110,8 +87,6 @@ export default function Example() {
                                                 <div className="relative rounded-md shadow-sm">
                                                     <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                                                         <SearchIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-
-
                                                     </div>
                                                     <input
                                                         type="search"
@@ -145,6 +120,10 @@ export default function Example() {
                                                                 <span className="absolute  right-16 block h-3 w-3 rounded-full ring-2 ring-white bg-gray-300" />
                                                                 Inactivos
                                                             </a>
+                                                            <a onClick={() => { setFiltrotodos(true) }} className='hover:bg-indigo-100 block px-4 py-2 text-sm font-medium text-gray-900'>
+                                                                <span className="absolute  right-16 block h-3 w-3 rounded-full ring-2 ring-white bg-gray-300" />
+                                                                Todos
+                                                            </a>
                                                         </div>
                                                     </Menu.Items>
                                                 </Transition>
@@ -168,10 +147,42 @@ export default function Example() {
                                 {/*EMPLEADOS*/}
                                 <nav aria-label="Message list" className="min-h-0 flex-1 overflow-y-auto">
                                     <div className="bg-white shadow overflow-hidden sm:rounded-md">
-                                        {data && data.map((empleado) => (
-                                            (empleado.EsActivo == true && activo == false ? <AxImput></AxImput> : "")
+                                        <ul role="list" className="divide-y divide-gray-200">
+                                            {data && data.map((empleado) => (
+                                                (empleado.EsActivo == activo && activo == activo ?
+                                                    <li key={empleado.id}>
+                                                        <a onClick={() => { setIdEmpleado(empleado.id) }}
+                                                            className={(empleado.id == idEmpleado ? "bg-indigo-100" : "") + " block hover:bg-indigo-200"}>
+                                                            <div className="flex px-4 py-4 sm:px-6">
+                                                                <div className="min-w-0 flex-1 flex">
+                                                                    <div className="flex-shrink-0">
+                                                                        {
+                                                                            empleado.ImgURL
+                                                                                ? <img className="h-12 w-12 rounded-full" src={empleado.ImgURL} alt="" />
+                                                                                : <svg className="bg-indigo-300 text-white h-12 w-12 rounded-full" viewBox="0 0 20 20" fill="currentColor">
+                                                                                    <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
+                                                                                </svg>
+                                                                        }
 
-                                        ))}
+                                                                    </div>
+                                                                    <div className="min-w-0 flex-1 px-4 md:grid md:grid-cols md:gap-4">
+                                                                        <div>
+                                                                            <p className="text-sm font-medium text-indigo-600 truncate">{empleado.Nombres + ' ' + empleado.Apellidos}</p>
+                                                                            <p className="mt-2 flex text-sm text-gray-500">
+                                                                                <MailIcon className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
+                                                                                <span className="truncate">{empleado.Email}</span>
+                                                                            </p>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                                <div>
+                                                                    <ChevronRightIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                                                                </div>
+                                                            </div>
+                                                        </a>
+                                                    </li> : ""
+                                                )))}
+                                        </ul>
 
                                     </div>
                                 </nav>
