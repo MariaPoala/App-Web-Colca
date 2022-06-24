d80d78cb00be41425a8a20805684bd671a234b2a
MariaVilcapoma <i2011451@continental.edu.pe>
2022-06-24 16:05:24 -0500

    cambio

--- components/ax-inicio.tsx
+++ components/ax-inicio.tsx
@@ -6,19 +6,21 @@ export default function AxInicio({ nombre, icon }: any) {
       <div className="min-h-full pt-16 pb-12 flex flex-col bg-white">
         <main className="flex-grow flex flex-col justify-center max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex-shrink-0 flex justify-center">
-            <Link href="/" className="inline-flex">
-              <span className="sr-only">Workflow</span>
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                <path strokeLinecap="round" strokeLinejoin="round"
-                   d = {nombre == "Distrito" ?"M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" : nombre == "Empleado" ?  "M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" :
-                    nombre == "Ciudadano" ?  "M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" :
-                      nombre == "Requisito" ? "M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4" : ""
-                  }>
-                </path>
+            <Link href="/">
+              <a href="/" className="inline-flex">
+                <span className="sr-only">Workflow</span>
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                  <path strokeLinecap="round" strokeLinejoin="round"
+                    d={nombre == "Distrito" ? "M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" : nombre == "Empleado" ? "M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" :
+                      nombre == "Ciudadano" ? "M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" :
+                        nombre == "Requisito" ? "M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4" : ""
+                    }>
+                  </path>
 
-              </svg>
+                </svg>
 
 
+              </a>
             </Link>
           </div>
           <div className="py-16">
@@ -34,7 +36,7 @@ export default function AxInicio({ nombre, icon }: any) {
               {"Para agregar un nuevo " + nombre}
               <p>  hacer clic en  <button type="button" className="bg-indigo-200 p-1 rounded-full text-indigo-500  ">
                 <span className="sr-only ">{"Agregar " + nombre}</span>
-                {(nombre == "Empleado" || nombre=="Ciudadano") ? <UserAddIcon className="h-5 w-5 border-solid " aria-hidden="true" /> :
+                {(nombre == "Empleado" || nombre == "Ciudadano") ? <UserAddIcon className="h-5 w-5 border-solid " aria-hidden="true" /> :
                   < PlusIcon className="h-5 w-5 border-solid " aria-hidden="true" />}
 
               </button> </p>
