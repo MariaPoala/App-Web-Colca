5aa93639385ea48c26eed5c425d38207d9f8323c
MariaVilcapoma <i2011451@continental.edu.pe>
2022-07-08 21:25:54 -0500

    cambio

--- pages/pruebadoc.tsx
+++ pages/pruebadoc.tsx
@@ -33,7 +33,7 @@ export default function Example() {
       <main className="flex-1 pb-8">
         <div className="mt-8">
           <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-            <h2 className="text-lg leading-6 font-medium text-gray-900">Overview</h2>
+            <h2 className="text-lg leading-6 font-medium text-gray-900">Documentos</h2>
             <div className="mt-2 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
               {/* Card */}
               {(listaDoc && listaDoc.map((item: any) =>
@@ -67,7 +67,6 @@ export default function Example() {
                 </div>
               )))
               )
-
               }
             </div>
           </div>
