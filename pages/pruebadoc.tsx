5d36ea032369ac494c294459a63b3e87557ca1aa
MariaVilcapoma <i2011451@continental.edu.pe>
2022-07-12 22:46:09 -0500

    cambio

--- pages/pruebadoc.tsx
+++ pages/pruebadoc.tsx
@@ -3,8 +3,7 @@ import { Fragment, useEffect, useRef, useState } from 'react'
 import RegistroDocumentoModel from 'models/registro-documento-model'
 import useSWRImmutable from "swr/immutable"
 import { AxInput, AxSelect, AxBtnEditar } from 'components/ax-form';
-import { CheckCircleIcon, DocumentAddIcon, FilterIcon, PlusIcon, XIcon } from '@heroicons/react/outline';
-import { CheckIcon } from '@heroicons/react/outline'
+import { CheckCircleIcon, DocumentAddIcon, FilterIcon, PlusIcon, XIcon, ExclamationCircleIcon } from '@heroicons/react/outline';
 import { Dialog, Transition } from '@headlessui/react';
 import AxRegistroDocumento from 'components/documento/ax-registro-documento'
 import { EnumEstadoEdicion, EnumTipoEdicion, TypeFormularioProps } from 'lib/edicion';
@@ -13,6 +12,21 @@ const fetcherDoc = (url: string): Promise<any> =>
 const fetcherCiudadano = (url: string): Promise<any> =>
   fetch(url, { method: "GET" }).then(r => r.json());
 
+const campos = [
+  { name: '' },
+  { name: 'N° Documento' },
+  { name: 'Observaciones' },
+  { name: 'Archivo' },
+  { name: 'Empleado' },
+  { name: 'Ciudadano' },
+  { name: 'Documento' },
+  { name: 'Fecha Registro' },
+  { name: 'Fecha Edicion' },
+  { name: 'Fecha Documento' },
+  { name: 'Fecha Anulación' },
+  { name: 'Motivo' }
+]
+
 export default function Example() {
   const { data: listaDoc } = useSWRImmutable('/api/documento/edicion', fetcherDoc);
   const { data: listaCiudadano } = useSWRImmutable('/api/ciudadano/edicion', fetcherCiudadano);
@@ -20,12 +34,15 @@ export default function Example() {
   const [listaRegistroDocumento, setListaRegistroDocumento] = useState<RegistroDocumentoModel[]>([]);
   const [abrir, setabrir] = useState(false)
   const cancelButtonRef = useRef(null)
-  const [tipoEdicion, setTipoEdicion] = useState(EnumTipoEdicion.VISUALIZAR)
   const [estadoEdicion, setEstadoEdicion] = useState(EnumEstadoEdicion.LISTAR)
   const [isLoading, setIsLoading] = useState(true);
   const [clic, setClic] = useState(false);
   const [grupo, setGrupo] = useState("");
-  const [texto, setTexto] = useState("");
+  const [textoNroDoc, setTextoNroDoc] = useState("");
+  const [textoCiudadano, setTextoCiudadano] = useState("");
+  const [textoFecha, setTextoFecha] = useState("");
+
+
 
 
   useEffect(() => {
@@ -49,33 +66,22 @@ export default function Example() {
       }
     });
 
-  const handleChange = (event:any) => setTexto(event.target.value)
+  const handleChange = (event: any) => {
+    if (event.target.name == "NroDocumento") {
+      setTextoNroDoc(event.target.value)
+    }
+    else if (event.target.name == "ciudadano") {
+      setTextoCiudadano(event.target.value)
+    }
+    else if (event.target.name == "Fecha") {
+      setTextoFecha(event.target.value)
+    }
+  }
 
-  
-  // const listaFiltro = ((texto == "" ? listaRegistroDocumento : listaRegistroDocumento.map((item) => (
-  //   "/documento/" + grupo == item.IDDocumento || texto == item.NroDocumento ?
-  //     listaRegistroDocumento.filter(doc =>
-  //       (doc.NroDocumento.toUpperCase().includes(texto.toUpperCase()) || doc.NroDocumento.toUpperCase().includes(texto.toUpperCase()))
-  //     ) : listaRegistroDocumento
-  // ))))
-
-  const listaFiltro = ((texto == "" ? listaRegistroDocumento : listaRegistroDocumento.map((item) => (
-    "/documento/" + grupo == item.IDDocumento || texto == item.NroDocumento &&
-    listaRegistroDocumento.filter(doc =>
-      (doc.NroDocumento.toUpperCase().includes(texto.toUpperCase()) || doc.NroDocumento.toUpperCase().includes(texto.toUpperCase()))
-    )
-  ))))
-
-  const mmm = ((grupo == "" ? listaRegistroDocumento :
-    listaRegistroDocumento.filter(doc =>( doc.IDDocumento == "/documento/" + grupo 
-    
-   && (clic==false ? listaRegistroDocumento :( texto=="" ?listaRegistroDocumento :  (doc.NroDocumento.toUpperCase().includes(texto.toUpperCase()) ))
-   ))))
-      
-  )
-  console.log(clic)
-  
-
+  const listaFiltro = (listaRegistroDocumento && listaRegistroDocumento.filter(doc => (doc.IDDocumento == "/documento/" + grupo
+    && (clic == false ? listaRegistroDocumento : (textoNroDoc == "" || textoFecha == "" ? listaRegistroDocumento : (doc.NroDocumento.toUpperCase().includes(textoNroDoc.toUpperCase())) &&
+      (doc.FecDocumento.toUpperCase().includes(textoFecha.toUpperCase())))
+    ))))
 
   return (
     <>
@@ -90,30 +96,23 @@ export default function Example() {
                     aria-hidden="true"
                   />
                   Filtrar Por:
-
                 </dd>
-
                 <div className="mt-2 grid px-16 grid-cols-1   gap-y-6 gap-x-4 md:grid-cols-6">
-                  <div className="md:col-span-1">
-                    <AxSelect name="ciudadano" value="" filtro={true} label="Ciudadanos" >
+                  <div className="md:col-span-2">
+                    <AxSelect name="ciudadano" value="" filtro={true} label="Ciudadanos" handleChange={handleChange}>
                       {listaCiudadano && listaCiudadano.map((ciudadano: any) => <option key={ciudadano.ID} value={"/grupo/" + ciudadano.ID}>{ciudadano.Nombres}</option>)}
                     </AxSelect>
                   </div>
                   <div className="md:col-span-1">
-                    <AxInput name="NroDocumento" handleChange={handleChange} label="Nro Documento"   type="text" filtro={true} />
-                  </div>
-                  <div className="md:col-span-1">
-                    <AxInput name="Fecha" label="Fecha de Documento" value="" filtro={true} type="date" />
+                    <AxInput name="NroDocumento" handleChange={handleChange} label="Nro Documento" type="text" filtro={true} />
                   </div>
                   <div className="md:col-span-2">
+                    <AxInput name="Fecha" label="Fecha de Documento" handleChange={handleChange} filtro={true} type="date" />
                   </div>
                   <div className="md:col-span-1">
-                    <button type="button" 
+                    <button type="button"
                       onClick=
-                      {() => {      setClic(true)   
-                        , mmm   
-                            
-                      }}
+                      {() => { setClic(true), listaFiltro }}
                       className="ml-3 h-8 mt-0 w-20 bottom-0 right-0  inline-flex items-center px-3 py-2 border 
                                             border-gray-300 shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500
                                             disabled:bg-blue-300"
@@ -122,10 +121,7 @@ export default function Example() {
                     </button>
                   </div>
                 </div>
-                {/* Profile */}
-
               </div>
-
             </div>
           </div>
         </div>
@@ -179,7 +175,7 @@ export default function Example() {
                 {() => {
                   setIdRegistroDocumento(IDRegistroDocumento);
                   setEstadoEdicion(EnumEstadoEdicion.SELECCIONADO);
-                  setabrir(true);
+                  { IDRegistroDocumento != "$NULL" && setabrir(true) }
                 }}
                 className="ml-3    inline-flex items-center px-3 py-2 border 
                                             border-gray-300 shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500
@@ -203,148 +199,92 @@ export default function Example() {
                 <DocumentAddIcon className="h-4 w-4 mr-1 text-white" aria-hidden="true" />
                 Agregar
               </button>
-
-
             </div>
           </div>
           <div className="hidden sm:block">
             <div className="mx-auto px-14 sm:px-16 lg:px-16">
               <div className="flex flex-col mt-2">
                 <div className="align-middle min-w-full overflow-x-auto shadow overflow-hidden sm:rounded-lg">
-                  <table className="min-w-full divide-y divide-gray-200">
-                    <thead>
-                      <tr>
-                        <th scope="col" className="relative w-12 px-6 sm:w-16 sm:px-8">
-
-                        </th>
-                        <th
-                          className="px-2 py-3 bg-gray-50 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
-                          scope="col"
-                        >
-                          N° Documento
-                        </th>
-                        <th
-                          className="px-6 py-3 bg-gray-50 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
-                          scope="col"
-                        >
-                          Observaciones
-                        </th>
-                        <th
-                          className=" px-6 py-3 bg-gray-50 text-center text-xs font-medium text-gray-500 uppercase tracking-wider "
-                          scope="col"
-                        >
-                          Archivo
-                        </th>
-                        <th
-                          className="px-6 py-3 bg-gray-50 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
-                          scope="col"
-                        >
-                          Empleado
-                        </th>
-
-                        <th
-                          className="px-6 py-3 bg-gray-50 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
-                          scope="col"
-                        >
-                          Ciudadano
-                        </th>
-                        <th
-                          className="px-6 py-3 bg-gray-50 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
-                          scope="col"
-                        >
-                          Documento
-                        </th>
-                        <th
-                          className=" px-6 py-3 bg-gray-50 text-center text-xs font-medium text-gray-500 uppercase tracking-wider "
-                          scope="col"
-                        >
-                          Fecha Registro
-                        </th>
-                        <th
-                          className="px-6 py-3 bg-gray-50 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
-                          scope="col"
-                        >
-                          Fecha Edicion
-                        </th>
-
-                        <th
-                          className="px-6 py-3 bg-gray-50 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
-                          scope="col"
-                        >
-                          Fecha Documento
-                        </th>
-                        <th
-                          className=" px-6 py-3 bg-gray-50 text-center text-xs font-medium text-gray-500 uppercase tracking-wider "
-                          scope="col"
-                        >
-                          Fecha Anulación
-                        </th>
-                        <th
-                          className="px-6 py-3 bg-gray-50 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
-                          scope="col"
-                        >
-                          Motivo
-                        </th>
-                      </tr>
-                    </thead>
-                    <tbody className="bg-white divide-y divide-gray-200">
-                      {mmm && mmm.map((item) => (
-                        <tr key={item.ID} className="bg-white">
-
-                          <td className="px-6 py-3 text-center whitespace-nowrap text-sm text-gray-900">
-                            <input
-                              onClick={() => {
-                                setIdRegistroDocumento(item.ID);
-                                setEstadoEdicion(EnumEstadoEdicion.SELECCIONADO);
-                              }}
-                              id="comments"
-                              aria-describedby="comments-description"
-                              name="comments"
-                              type="checkbox"
-                              className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
-                            />
-                          </td>
-                          <td className="px-6 py-3 text-center whitespace-nowrap text-sm text-gray-500">
-                            {item.NroDocumento}
-                          </td>
-                          <td className="px-6 py-3 text-center whitespace-nowrap text-sm text-gray-500">
-                            {item.Observacion}
-                          </td>
-                          <td className="px-6 py-3 text-center whitespace-nowrap text-sm text-gray-500">
-                            {item.URLArchivo}
-                          </td>
-                          <td className="px-6 py-3 text-center whitespace-nowrap text-sm text-gray-500">
-                            {item.IDEmpleado}
-                          </td>
-
-                          <td className="px-6 py-3 text-center whitespace-nowrap text-sm text-gray-500">
-                            {item.IDCiudadano}
-                          </td>
-                          <td className="px-6 py-3 text-center whitespace-nowrap text-sm text-gray-500">
-                            {item.IDDocumento}
-                          </td>
-                          <td className="px-6 py-3 text-center whitespace-nowrap text-sm text-gray-500">
-                            {item.FecRegistro}
-                          </td>
-                          <td className="px-6 py-3 text-center whitespace-nowrap text-sm text-gray-500">
-                            {item.FecEdicion}
-                          </td>
-                          <td className="px-6 py-3 text-center whitespace-nowrap text-sm text-gray-500">
-                            {item.FecDocumento}
-                          </td>
-                          <td className="px-6 py-3 text-center whitespace-nowrap text-sm text-gray-500">
-                            {item.FecAnulacion}
-                          </td>
-                          <td className="px-6 py-3 text-center whitespace-nowrap text-sm text-gray-500">
-                            {item.Motivo}
-                          </td>
+                  {grupo == "" ?
+                    <dd className="mt-10 gap-2 p-10 flex items-center text-sm text-gray-500 font-medium sm:mr-6 sm:mt-0 capitalize">
+                      <ExclamationCircleIcon
+                        className="flex-shrink-0 mr-0 h-5 w-5 text-orange-500"
+                        aria-hidden="true"
+                      />
+                      ¡Seleccionar un Documento!
+                    </dd> :
+                    <table className="min-w-full divide-y divide-gray-200">
+                      <thead>
+                        <tr >
+                          {campos.map((item) => (
+                            <th key={item.name}
+                              className="px-2 py-3 bg-gray-50 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                              scope="col"
+                            >
+                              {item.name}
+                            </th>
+                          ))}
                         </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                  {/* Pagination */}
+                      </thead>
 
+                      <tbody className="bg-white divide-y divide-gray-200">
+                        {(listaFiltro && listaFiltro.map((item) => (
+
+                          <tr key={item.ID} className="bg-white">
+
+                            <td className="px-6 py-3 text-center whitespace-nowrap text-sm text-gray-900">
+                              <input
+                                onClick={() => {
+                                  setIdRegistroDocumento(item.ID);
+                                  setEstadoEdicion(EnumEstadoEdicion.SELECCIONADO);
+                                }}
+                                id="comments"
+                                aria-describedby="comments-description"
+                                name="comments"
+                                type="checkbox"
+                                className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
+                              />
+                            </td>
+                            <td className="px-6 py-3 text-center whitespace-nowrap text-sm text-gray-500">
+                              {item.NroDocumento}
+                            </td>
+                            <td className="px-6 py-3 text-center whitespace-nowrap text-sm text-gray-500">
+                              {item.Observacion}
+                            </td>
+                            <td className="px-6 py-3 text-center whitespace-nowrap text-sm text-gray-500">
+                              {item.URLArchivo}
+                            </td>
+                            <td className="px-6 py-3 text-center whitespace-nowrap text-sm text-gray-500">
+                              {item.IDEmpleado}
+                            </td>
+
+                            <td className="px-6 py-3 text-center whitespace-nowrap text-sm text-gray-500">
+                              {item.IDCiudadano}
+                            </td>
+                            <td className="px-6 py-3 text-center whitespace-nowrap text-sm text-gray-500">
+                              {item.IDDocumento}
+                            </td>
+                            <td className="px-6 py-3 text-center whitespace-nowrap text-sm text-gray-500">
+                              {item.FecRegistro}
+                            </td>
+                            <td className="px-6 py-3 text-center whitespace-nowrap text-sm text-gray-500">
+                              {item.FecEdicion}
+                            </td>
+                            <td className="px-6 py-3 text-center whitespace-nowrap text-sm text-gray-500">
+                              {item.FecDocumento}
+                            </td>
+                            <td className="px-6 py-3 text-center whitespace-nowrap text-sm text-gray-500">
+                              {item.FecAnulacion}
+                            </td>
+                            <td className="px-6 py-3 text-center whitespace-nowrap text-sm text-gray-500">
+                              {item.Motivo}
+                            </td>
+                          </tr>
+                        )))}
+                      </tbody>
+                    </table>}                  
                 </div>
+                {/* Pagination */}
                 <nav
                   className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6"
                   aria-label="Pagination"
