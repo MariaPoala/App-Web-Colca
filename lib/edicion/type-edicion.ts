9c23efd548c8202287d95932af01bb04cba02bfb
Victor Acuña Sulluchuco <veas.v2@gmail.com>
2022-07-13 15:29:04 -0500

    comidian

--- lib/edicion/type-edicion.ts
+++ lib/edicion/type-edicion.ts
@@ -1,16 +1,15 @@
 import { EnumEstadoEdicion } from "./enum-estado-edicion";
 import { EnumTipoEdicion } from "./enum-tipo-edicion";
 type TypeSetID = (ID: string) => any;
-type TypeTipoEdicion = (tipoEdicionn: EnumTipoEdicion) => any;
 type TypeSetEstadoEdicion = (luegoEdicion: EnumEstadoEdicion) => any;
 type TypeSetTipoEdicion = (settipoEdicion: EnumTipoEdicion) => any;
+
 type TypeFormularioProps = {
     ID: string
     setID: TypeSetID,   
     setEstadoEdicion: TypeSetEstadoEdicion,
+    tipoEdicion: EnumTipoEdicion
     setTipoEdicion: TypeSetTipoEdicion,
-    tipoEdicion: TypeTipoEdicion
-
 }
 
 export type {TypeFormularioProps};
