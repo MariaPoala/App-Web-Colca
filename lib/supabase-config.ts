985be93d9ced106a07d83546b1909415bbee9624
MariaVilcapoma <i2011451@continental.edu.pe>
2022-07-21 16:41:55 -0500

    cambios

--- lib/supabase-config.ts
+++ lib/supabase-config.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 const supabase = createClient(
-    'https://bhskhuzpkiyxzefgqbgz.supabase.co', 
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJoc2todXpwa2l5eHplZmdxYmd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTg0MjMwOTcsImV4cCI6MTk3Mzk5OTA5N30.XncUftDU-rUgnr9Ef_upkA9xZZk8TVSoLiIJenRS1yI')
+    'https://fmawnrfwepqksamtepfh.supabase.co', 
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZtYXducmZ3ZXBxa3NhbXRlcGZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTg0MzA5ODMsImV4cCI6MTk3NDAwNjk4M30.gAAc53JoGodoqFUdcIwcFLT8InM8c3e3ouRd5kHbaWY')
 
 export default supabase;
 
