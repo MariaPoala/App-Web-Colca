c1026ff01e74a2e82f142bbd94b3a66cfaca988b
Victor Acuña Sulluchuco <veas.v2@gmail.com>
2022-07-21 14:10:37 -0500

    comidian

--- pages/api/Distrito/edicion.ts
+++ pages/api/Distrito/edicion.ts
@@ -2,7 +2,7 @@ import FnSaveData from "lib/database/save_data";
 import DistritoModel from "models/distrito_model";
 
 export default async function handler(req: any, res: any) {
-    const { data, error } = await FnSaveData<DistritoModel>(req.method, req.body);
+    const { data, error } = await FnSaveData<DistritoModel>("distrito", req.method, req.body);
     if (error) {
         res.status(401).json(error);
     }
