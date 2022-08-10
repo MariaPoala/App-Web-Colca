56a6e591434f878096ab5d4b57aafdf22b1e6f87
MariaVilcapoma <i2011451@continental.edu.pe>
2022-08-09 22:04:39 -0500

    cambio

--- pages/documento/documento.tsx
+++ pages/documento/documento.tsx
@@ -180,7 +180,6 @@ export default function AxPageDocumento() {
           <div className="max-w-6xl mx-16 px-2 sm:px-2 ">
             <h2 className="text-lg leading-6 font-medium text-gray-900">Documentos</h2>
             <div className="mt-2 grid  gap-5 grid-cols-1 sm:grid-cols-2  md:grid-cols-3  lg:grid-cols-5">
-              {/* Card */}
               {(listaTipoDocumento && listaTipoDocumento.map((item: any) =>
               (resultado.map(s => s.id == item.id &&
                 <ul key={item.id} className="bg-indigo-400 cursor-pointer overflow-hidden shadow rounded-lg hover:bg-indigo-700"
